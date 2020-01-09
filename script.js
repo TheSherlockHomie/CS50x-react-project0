b0363e9a47efdbb80a75113cd262612adcca60c3
TheSherlockHomie <coolman.kushagra@gmail.com>
2020-01-09 23:11:24 +0530

    New TODO button now works!

--- script.js
+++ script.js
@@ -10,5 +10,22 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-  alert('New TODO button clicked!')
+  //take input to do
+  const inputText = prompt("What would you like to do?")
+  //make  new list item
+  const newItem = document.createElement("li")
+  //make check box
+  const newCheckBox = document.createElement("input")
+  newCheckBox.setAttribute("type", "checkbox")
+  newCheckBox.className = "todo-checkbox"
+  //insert this in list
+  newItem.appendChild(newCheckBox)
+  //insert text in form
+  const txt = document.createTextNode(inputText)
+  const span = document.createElement("span")
+  span.appendChild(newCheckBox)
+  span.appendChild(txt)
+  newItem.appendChild(span)
+  //insert item in list
+  list.appendChild(newItem)
 }
